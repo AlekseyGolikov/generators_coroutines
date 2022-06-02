c0762d0460f7e1041bd5c27bbe774f3679809bd9
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-06-02 20:53:49 +0300

    update gen-coro.py coroutine in class-wrapper

--- gen-coro.py
+++ gen-coro.py
@@ -19,15 +19,33 @@ def coroutine():
     except GeneratorExit:
         pass
 
+class Coro:
+    """Класс реализует обращение к сопрограмме"""
+    def __init__(self, coroutine, *args):
+        self._coro = coroutine()
+    def __enter__(self):
+        self._coro.__next__()
+        return self._coro
+    def __exit__(self, type, value, traceback):
+        self._coro.close()
+        if value is None:
+            return True
+        return False
+    def send(self, *args):
+        return self._coro.send(*args)
+
 def copy():
     g = generator()
-    c = coroutine()
-    c.__next__()
+    # c = coroutine()
+    # c.__next__()
     while 1:
         try:
             n = g.__next__()
-            c.send(n)
             print(n)
+            with Coro(coroutine, n) as coro:
+                coro.send(n)
+            # c.send(n)
+
             sleep(1)
         except StopIteration:
             print('Запись окончена!')
