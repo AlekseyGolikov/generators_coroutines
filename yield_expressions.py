9018ed062fe56cb9e53ca800872da403ad5ee1de
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-06-02 17:22:44 +0300

    update yield_expressions.py

--- yield_expressions.py
+++ yield_expressions.py
@@ -1,25 +1,37 @@
 # Пример реалзиации yield выражения
 #         Вывод:
-#         Готов к приему значний:
-#         ведите значение: str1
-#         Получено значение: str1
-#         ведите значение: str2
-#         Получено значение: str2
-#         ведите значение: str3
-#         Получено значение: str3
-#         ведите значение:
+#         Готов к приему значений
+#         Введите значение: 323
+#         Получено 323
+#         Введите значение: we
+#         Получено we
+#         Введите значение: exit
+#         Получено exit
+#         Передача закончена!
+#
+#         Process finished with exit code 0
 
+def coroutine(func):
+    def wrapper(*args):
+        g = func(*args)
+        g.__next__()
+        return g
+    return wrapper
+
+@coroutine
 def receiver():
-    print('Готов к приему значний:')
-    while 1:
-        n = (yield)
-        print('Получено значение: %s' % n)
+    print('Готов к приему значений')
+    while True:
+        str = (yield)
+        print('Получено %s' % str)
+        if str == 'exit':
+            raise GeneratorExit
+
 
 r = receiver()
-r.__next__()
-while True:
+while 1:
     try:
-        r.send(input('ведите значение: '))
-    except KeyboardInterrupt:
-        print('Передача окончена!')
+        r.send(input('Введите значение: '))
+    except GeneratorExit:
         break
+print('Передача закончена!')
